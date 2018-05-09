9a54a2674b40f2235db5dd369c289cff0c80480b
alexandre.assis <alexandre.assis@mobicare.com.br>
2018-05-09 13:40:15 -0300

    ajustes

--- src/app/menu-day/menu-day.component.ts
+++ src/app/menu-day/menu-day.component.ts
@@ -17,6 +17,10 @@ export class MenuDayComponent implements OnInit {
   id: number;
   menu: Menu;
   day: Day;
+  salad1 = false;
+  salad2 = false;
+  salad3 = false;
+  
   constructor(private route: ActivatedRoute, private menuComponent: MenuComponent) { }
 
   ngOnInit() {
@@ -24,4 +28,31 @@ export class MenuDayComponent implements OnInit {
     this.day = this.menuComponent.getDay(this.id);  
   }
 
+  saladItemPanel(op: number): string {
+    if(this.saladCount() == 2){
+      if(op==1)
+        return 'salad-item-panel50_1';
+       else if(op == 2){
+         return 'salad-item-panel50_2';
+       }
+       else{
+        return 'salad-item-panel50_3';
+       } 
+    }else{
+      return 'salad-item-panel100';
+    }
+  }
+  saladCount(): number{
+    let c =0;
+    if(this.salad1)
+      c++;
+    
+    if(this.salad2)
+      c++;
+   
+    if(this.salad3)
+      c++;
+     
+      return c;
+  }
 }
